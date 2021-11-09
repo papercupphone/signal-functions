505b820cc7ab50d8764c91e547b8ecf1a54c8b6d
ekinoncu <>
2021-11-09 14:03:15 +0300

    leave function added

---
+++ src/leave.ts
@@ -0,0 +1,19 @@
+import Dao from './dao'
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    if(event.body){
+        let body = JSON.parse(event.body)
+        let dao = new Dao()
+        let user = await dao.getUser(body.connectionId)
+        if (user) {
+            await dao.deleteUserFromRoom(user.Item)
+            await dao.deleteFromUsers(body.connectionId)
+            return {statusCode: 200, body: 'Disconnected.'}
+        } else {
+            return {statusCode: 400, body: 'User not found.'}
+        }
+    }else{
+        return {statusCode: 400, body: 'Bad request'}
+    }
+}
